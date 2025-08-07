a63d99d2f09e9826f622c41f51ba61d68a0ec97f
Javier Huertas <javiandre2007@gmail.com>
2025-08-07 11:02:28 -0600

    Opcion 2

--- Actividad 16.py
+++ Actividad 16.py
@@ -13,10 +13,21 @@ def add_libro():
         autor = input("Agregue el autor del libro: ").upper()
         publi = int(input("Ingrese el año de publicacion del libro: "))
         libro = Libro(titulo,autor,publi)
-        books.append(books)
+        books.append(libro)
     except ValueError:
         print("Ingrese un valor valido")
     except TypeError:
         print("Ingrese un tipo de valor valido")
     except Exception as e:
         print("Un error inesperado ha ocurrido")
+
+def mostrar_lista():
+    if not books:
+        print("Ningun libro registrado, Registre primero un libro")
+    else:
+        print("Lista de libros: ")
+        i = 1
+        for book in books:
+            print(f"{i}.", end= "")
+            book.show_book()
+        print()
