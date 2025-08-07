6ec5ee35ac316c1601c2c62132030c1374ca162a
Javier Huertas <javiandre2007@gmail.com>
2025-08-07 09:53:53 -0600

    Opcion 1

--- Actividad 16.py
+++ Actividad 16.py
@@ -7,4 +7,16 @@ class Libro:
         print(f"El libro {self.titulo} ha sido agregado correctamente")
 
 books = []
-
+def add_libro():
+    try:
+        titulo = input("Ingrese el titulo del libro: ").upper()
+        autor = input("Agregue el autor del libro: ").upper()
+        publi = int(input("Ingrese el año de publicacion del libro: "))
+        libro = Libro(titulo,autor,publi)
+        books.append(books)
+    except ValueError:
+        print("Ingrese un valor valido")
+    except TypeError:
+        print("Ingrese un tipo de valor valido")
+    except Exception as e:
+        print("Un error inesperado ha ocurrido")
