f21c421722f3ee4fdc76790421176e71018954b7
Javier Huertas <javiandre2007@gmail.com>
2025-08-07 11:14:37 -0600

    Arreglar la opcion de mostrar

--- Actividad 16.py
+++ Actividad 16.py
@@ -4,14 +4,16 @@ class Libro:
         self.autor = autor
         self.publicacion = publicacion
     def show_book(self):
-        print(f"El libro {self.titulo} ha sido agregado correctamente")
-
+        print(f"Libro: {self.titulo}\n"
+              f"Autor: {self.autor}\n"
+              f"Año de publicacion: {self.publicacion}")
 books = []
 def add_libro():
     try:
         titulo = input("Ingrese el titulo del libro: ").upper()
         autor = input("Agregue el autor del libro: ").upper()
         publi = int(input("Ingrese el año de publicacion del libro: "))
+        print(f"El libro: "f"{titulo} " + "ha sido agregado correctamente.")
         libro = Libro(titulo,autor,publi)
         books.append(libro)
     except ValueError:
@@ -22,15 +24,22 @@ def add_libro():
         print("Un error inesperado ha ocurrido")
 
 def mostrar_lista():
-    if not books:
-        print("Ningun libro registrado, Registre primero un libro")
-    else:
-        print("Lista de libros: ")
-        i = 1
-        for book in books:
-            print(f"{i}.", end= "")
-            book.show_book()
-        print()
+    try:
+        if not books:
+            print("Ningun libro registrado, Registre primero un libro")
+        else:
+            print("Lista de libros: ")
+            i = 1
+            for mostrar in books:
+                print(f"{i}.", end= "")
+                mostrar.show_book()
+            print()
+    except ValueError:
+        print("Ingrese un valor valido")
+    except TypeError:
+        print("Ingrese un tipo de valor valido")
+    except Exception as e:
+        print("Un error inesperado ha ocurrido")
 
 def remove_book():
     try:
@@ -52,9 +61,9 @@ def remove_book():
         print("Un error inesperado ha ocurrido")
 while True:
     print("-"*10 + "Bienvenido al registro de Libros"+ "-"*10 + "\n"
-        "1. Registrar Libros."
-        "2. Mostrar libros."
-        "3.Eliminar libro por nombre."
+        "1. Registrar Libros.\n"
+        "2. Mostrar libros.\n"
+        "3.Eliminar libro por nombre.\n"
         "4. Salir del programa")
     user_op = input("Ingrese la opcion que desea ingresar: ")
     match user_op:
@@ -65,6 +74,7 @@ while True:
         case "3":
             credits()
         case "4":
+            print("¡Gracias por usar el programa!")
             break
         case _:
             print("Ingrese un dato valido")
